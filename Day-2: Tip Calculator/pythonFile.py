56586d7c8018794141427d5f956f8bd0cc935c3f
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-02 19:54:31 +0530

    Update pythonFile.py

--- Day-2: Tip Calculator/pythonFile.py
+++ Day-2: Tip Calculator/pythonFile.py
@@ -1,1 +1,17 @@
+# Tip Calculator
 
+# Welcome Message
+print("Welcome to the tip Calculator!\n\n")
+
+# taking inputs
+bill=int(input("What was your total bill?\nRs "))
+tip=int(input("What percentage of tip do you want to give?(10,12,15)\n"))
+n=int(input("How many people are splitting the bill?\n"))
+
+# Calculating individual bills
+s= (bill*tip)/(100)
+s+=bill
+s/=n
+s=round(s,2)
+
+print(f"Each person should pay Rs {s}")

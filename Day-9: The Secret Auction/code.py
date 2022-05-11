9e218f69068d1b5836efd1997151942b0de1a66f
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-11 20:10:29 +0530

    Update code.py

--- Day-9: The Secret Auction/code.py
+++ Day-9: The Secret Auction/code.py
@@ -1,1 +1,27 @@
+from replit import clear    #FOR CLEAR() FUNCTION
 
+from art import logo
+print(logo)
+
+c=True
+d={}
+
+while c:
+  name=input("What's your name?\n")
+  bid=input("Your bid is?\n")
+  d[name]=bid
+  # print(d)
+  p=input("Are there anymore bidders? 'yes' or 'no'\n").lower()
+  if p=='no':
+    c=False
+  clear()
+
+min=0
+n=""
+
+for key in d:
+  if int(d[key])>min:
+    min=int(d[key])
+    n=key
+
+print(f"{n} won the bid with {min}")
